aedf2242ad63802e38cf75d1c93d9f821b91a15f
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2018-10-11 16:35:39 +0200

    I counted wrong.
    What happened to me ? I never count bad.

--- password.c
+++ password.c
@@ -2,7 +2,7 @@
 #include <time.h>
 char pass[];
 int random, len, choice, seed = 0;
-char alphabet[70] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', 
+char alphabet[69] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', 
 '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*'};
 int main () {
 	printf("Do you want to be able to regenerate your password if you lose it ? (1 = Yes, 0 = No): ");
@@ -22,7 +22,7 @@ int main () {
 	printf("Enter lenghth of your desired password: ");
 	scanf("%d", &len);
 	for (int i = 0; i <= len - 1; i++) {
-		random = rand() % 71;
+		random = rand() % 70;
 		printf("%c", alphabet[random]);
 	}
 printf("\n");
